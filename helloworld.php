635769bf859b72b699f3766c579ec72936f1e2cc
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2015-09-09 11:37:19 +0300

    Matthias changed file

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,22 @@
 
 		echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+
+		$age = 6;		
+		
+		//testib loogikat, juhul kui vanus on väiksem kui 18 siis kirjuta "alaealine",
+		//muul juhul "täisealine"
+		//if sisse loogikatehe
+		if($age < 18) {
+			//tõene
+			
+			echo "alaealine";
+		} else {
+			//väär
+			
+			echo "täisealine";
+		}	
+			
+?>
