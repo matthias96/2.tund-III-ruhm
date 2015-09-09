15fcfaab80e961660145307a7242e7c4f7640dca
Matthias Johann Kurs <mattkur@greeny.cs.tlu.ee>
2015-09-09 12:14:49 +0300

    loop

--- helloworld.php
+++ helloworld.php
@@ -23,3 +23,19 @@
 		}	
 			
 ?>
+<br>
+<?php
+
+		//vastavalt vanusele trükime nii mitu korda välja sõna "palju"
+		
+		for($i = 0; $i <$age; $i=$i + 1)	{
+			
+			//tegevus mis kordub
+			echo "palju".$i." ";
+			
+			
+		}
+		
+		echo "õnne!";
+
+?>
